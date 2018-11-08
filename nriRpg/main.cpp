83aa2d282f479128837d2fe86945f735f0d94451
Archodonata <ateist85@mail.ru>
2018-11-09 00:57:14 +0300

    добавил вывод отладочного сообщения

--- nriRpg/main.cpp
+++ nriRpg/main.cpp
@@ -1,11 +1,12 @@
 #include "mainnrirpg.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     mainNriRpg w;
     w.show();
-
+    qDebug()<<"hello debug";
     return a.exec();
 }
